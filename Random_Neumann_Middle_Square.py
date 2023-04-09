1adc65625d86870363004b18b431cd2601911339
MihaiTraista <mihai.musicmail@gmail.com>
2023-04-09 18:26:43 +0100

    middle square

---
+++ Random_Neumann_Middle_Square.py
@@ -0,0 +1,6 @@
+# generating a pseudo random number based on von Neumann's the "middle square" method
+# https://en.wikipedia.org/wiki/Middle-square_method
+
+# challenge 1 write a function that takes a seed and returns the middle squared
+# challenge 2 calculate the period for any given seed for 8-digit numbers, 4-digit seed
+
