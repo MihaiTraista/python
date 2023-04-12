b06123f9fd95864c1bc09c8732cf7b43a8929a4c
MihaiTraista <mihai.musicmail@gmail.com>
2023-04-12 14:57:36 +0100

    linked list palindrome

--- Linked_Lists_4.py
+++ Linked_Lists_4.py
@@ -1,5 +1,5 @@
 #   Palindrome Linked List - Leetcode 234 - Python
-#   https://www.youtube.com/watch?v=yOzXms1J6Nk
+#   Write a function that receives a linked list and returns true if the list is a palindrome
 
 class Node:
     def __init__(self, data):
@@ -34,6 +34,7 @@ class LinkedList:
         return n
 
     def get_length_recursive(self, node):
+        #   O(n)
         if node is None:
             return 0
         else:
@@ -47,8 +48,30 @@ class LinkedList:
         print()
 
 
-def is_palindrome(list):
-    pass
+def peek(linked_list, b):
+    #   O(n)
+    runner = linked_list.head
+    while runner is not None:
+        if runner.next is b:
+            return runner
+        runner = runner.next
+
+
+def is_palindrome(linked_list):
+    #   O(n / 2) * O(n) = O(n^2)
+    if linked_list.get_length_recursive(linked_list.head) % 2 != 0:     # O(n)
+        return False
+    is_pali = True
+    a = linked_list.head
+    b = peek(linked_list, None)     # O(n)
+    while a.next is not b:          # O(n/2)
+        if a.data != b.data:
+            is_pali = False
+            break
+        a = a.next
+        b = peek(linked_list, b)    # O(n)
+
+    return is_pali
 
 
 my_list = LinkedList()
@@ -58,5 +81,5 @@ for value in values:
     my_list.append(value)
 
 my_list.print_list()
+print(f"get_length_recursive {my_list.get_length_recursive(my_list.head)}")
 print(f"isPalindrome {is_palindrome(my_list)}")
-print(f"len {my_list.get_length_recursive(my_list.head)}")
