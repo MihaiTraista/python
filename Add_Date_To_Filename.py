cd5715644f32626091ec958df0ba3664b725f4b5
MihaiTraista <mihai.musicmail@gmail.com>
2023-04-27 11:07:45 +0100

    filename

--- Add_Date_To_Filename.py
+++ Add_Date_To_Filename.py
@@ -7,4 +7,7 @@ dir_path = '/Users/mihaitraista/Desktop/test'
 
 for file_name in os.listdir(dir_path):
     if file_name.endswith('.m4a'):  # Only process m4a files (iOS voice memos)
-        pass
+        file_path = os.path.join(dir_path, file_name)
+        file_stat = os.stat(file_path)
+        create_time = file_stat.st_mtime  # Unix format
+        create_date = datetime.datetime.fromtimestamp(create_time).strftime('%Y-%m-%d')  # date string
