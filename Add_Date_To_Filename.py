b89a8be3154c69603b0d596929fc85179b26455a
MihaiTraista <mihai.musicmail@gmail.com>
2023-04-27 11:06:18 +0100

    filename

--- Add_Date_To_Filename.py
+++ Add_Date_To_Filename.py
@@ -1,7 +1,10 @@
-# prepend the date to the filename to all files in a directory
+# after importing all voice memos from iPhone, run this to prepend the date to the name of each file
 
 import os
 import datetime
 
 dir_path = '/Users/mihaitraista/Desktop/test'
 
+for file_name in os.listdir(dir_path):
+    if file_name.endswith('.m4a'):  # Only process m4a files (iOS voice memos)
+        pass
