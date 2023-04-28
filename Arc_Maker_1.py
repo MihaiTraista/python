a2ffb26bc2bf6167eb09d5f911707f289ad38430
MihaiTraista <mihai.musicmail@gmail.com>
2023-04-28 16:24:56 +0100

    arc

--- Arc_Maker_1.py
+++ Arc_Maker_1.py
@@ -10,3 +10,14 @@ color("orange")
 width(1)
 speed(0)
 tracer(10)
+
+
+def get_distance_between_two_points(p1, p2):
+    #   the square root of the sum of the squares of the other two sides
+    x1, y1 = p1
+    x2, y2 = p2
+    base = x2 - x1
+    height = y2 - y1
+    hypotenuse = math.sqrt(base ** 2 + height ** 2)
+    return hypotenuse
+
