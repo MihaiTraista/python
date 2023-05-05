c80f0870ca2f2dd718a610af8f004564cf587c96
MihaiTraista <mihai.musicmail@gmail.com>
2023-05-05 18:45:44 +0100

    star

--- Star_Drawing.py
+++ Star_Drawing.py
@@ -1,14 +1,1 @@
-import turtle
-
-# define turtle object
-turtle.colormode(255)
-tommy = turtle.Turtle()
-screen = turtle.Screen()
-tommy.shape("turtle")
-tommy.speed(0)
-
-# prepare for drawing
-tommy.penup()
-tommy.color("orange")
-screen.bgcolor(0, 40, 50)
 
