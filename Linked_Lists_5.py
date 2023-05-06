ecb233768f36ae434cac023d08578543a4aa2480
MihaiTraista <mihai.musicmail@gmail.com>
2023-05-06 18:42:10 +0100

    linked

--- Linked_Lists_5.py
+++ Linked_Lists_5.py
@@ -4,12 +4,30 @@ class Node(object):
         self.next = None
 
 
+def print_list(head):
+    if head is None:
+        return
+    node = head
+    while node is not None:
+        print(node.data, "-> ", end='')
+        node = node.next
+
 def push(head, data):
-    pass
+    if head is None:
+        return Node(data)
+    else:
+        new_head = Node(data)
+        new_head.next = head
+        return new_head
 
 
 def build_one_two_three():
     pass
 
 
-push()
+chained = None
+chained = push(chained, 3)
+chained = push(chained, 5)
+chained = push(chained, 7)
+
+print_list(chained)
