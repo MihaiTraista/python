169cb087093434556a0b607a048769951317e41c
MihaiTraista <mihai.musicmail@gmail.com>
2023-05-06 18:33:34 +0100

    linked

---
+++ Linked_Lists_5.py
@@ -0,0 +1,15 @@
+class Node(object):
+    def __init__(self, data):
+        self.data = data
+        self.next = None
+
+
+def push(head, data):
+    pass
+
+
+def build_one_two_three():
+    pass
+
+
+push()
