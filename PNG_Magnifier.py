9bd6006fbf783039660053edfae667a496e1f766
MihaiTraista <mihai.musicmail@gmail.com>
2023-05-10 09:41:43 +0100

    magnifier

--- PNG_Magnifier.py
+++ PNG_Magnifier.py
@@ -10,3 +10,17 @@ output_image = Image.new("RGB", (64, 64), "white")
 input_pixel = input_image.getpixel((0, 0))
 print(input_pixel)
 
+
+def stretch_wide():
+    for x in range(64):
+        for y in range(64):
+            input_coord = (x // 4 + 9, y // 2)
+            color = input_image.getpixel(input_coord)
+            # print(color)
+            output_coord = (x, y)
+            output_image.putpixel(output_coord, color)
+
+
+stretch_wide()
+# magnify(20)
+output_image.save("/Users/mihaitraista/Desktop/6464.png")
