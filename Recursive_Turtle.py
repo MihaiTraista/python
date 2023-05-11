6dbd36d61a7e864466c4e95a130553509bd47b84
MihaiTraista <mihai.musicmail@gmail.com>
2023-05-11 19:55:20 +0100

    recursive

--- Recursive_Turtle.py
+++ Recursive_Turtle.py
@@ -21,4 +21,33 @@ t.pendown()
 currentColor = [128, 128, 128]
 
 
+def slight_rand_color(amount, greyFlag):
+    if greyFlag:
+        currentColor[0] = (currentColor[0] + random.randint(amount*-1, amount)) % 256
+        currentColor[1] = currentColor[0]
+        currentColor[2] = currentColor[0]
+    else:
+        currentColor[0] = (currentColor[0] + random.randint(amount*-1, amount)) % 256
+        currentColor[1] = (currentColor[1] + random.randint(amount*-1, amount)) % 256
+        currentColor[2] = (currentColor[2] + random.randint(amount*-1, amount)) % 256
+    t.color(currentColor[0], currentColor[1], currentColor[2])
+
+
+def draw_recursive_hexagon(levels):
+    if levels == 1:
+        return
+    else:
+        size = (levels - 2) / 2
+        size = math.pow(size, 0.2) * 70 + 5
+        sides = 11-levels
+        #   print(f'levels = {levels}, math.pow(size, 0.3) = {math.pow(((levels - 2) / 2), 0.3)} size = {size},\t\t\t sides = {sides}')
+        for i in range(sides):
+            slight_rand_color(2, True)
+            t.forward(size)
+            draw_recursive_hexagon(levels-1)
+            t.left(360 / sides)
+
+
+draw_recursive_hexagon(6)
+
 turtle.done()
