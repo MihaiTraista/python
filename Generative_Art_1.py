0bacb437d716b3182e41aa81b43b63c18faf607c
MihaiTraista <mihai.musicmail@gmail.com>
2023-05-12 19:25:16 +0100

    gen art

--- Generative_Art_1.py
+++ Generative_Art_1.py
@@ -7,6 +7,22 @@ height = 901
 new_img = Image.new("RGBA", (width, height), "black")
 
 
+def draw_jittery_line(exponent):
+    x = 450
+    y = 450
+
+    offset = random.randint(0, 360)
+    color = (random.randint(0, 255), random.randint(0, 255), random.randint(0, 255), random.randint(0, 128))
+    # color = (255, 255, 255, random.randint(0, 128))
+
+
+
+
+for i in range(1, 500):
+    draw_jittery_line(math.pow(random.random(), 0.4))
+
+
+new_img.save("/Users/mihaitraista/Desktop/new_image.png")
 print("done!")
 
 
