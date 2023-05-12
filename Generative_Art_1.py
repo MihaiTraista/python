d9a2a02bb353fbd0655e3074ff167aaf48ecfe01
MihaiTraista <mihai.musicmail@gmail.com>
2023-05-12 19:25:40 +0100

    gen art

--- Generative_Art_1.py
+++ Generative_Art_1.py
@@ -15,7 +15,18 @@ def draw_jittery_line(exponent):
     color = (random.randint(0, 255), random.randint(0, 255), random.randint(0, 255), random.randint(0, 128))
     # color = (255, 255, 255, random.randint(0, 128))
 
-
+    for i in range(500):
+        if x >= 900 or y >= 900 or x <= 0 or y <= 0:
+            break
+        color = (color[0], color[1], color[2], int((500 - i) / 500 * 255))
+        # color = (color[0], color[1], color[2], 255)
+        angle = random.random()
+        angle = math.pow(angle, exponent)
+        angle *= 360
+        angle = (angle + offset) % 360
+        x += 1 * math.cos(math.radians(angle))
+        y += 1 * math.sin(math.radians(angle))
+        new_img.putpixel((int(x), int(y)), color)
 
 
 for i in range(1, 500):
